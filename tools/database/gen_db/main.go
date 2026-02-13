1eb2e756596582eb7fdcd3491af36cc51bd4b16d
Adrian Klingen <adrian.klingen@deptagency.com>
2026-02-13 12:07:47 +0100

    Fix SCC item inference

--- tools/database/gen_db/main.go
+++ tools/database/gen_db/main.go
@@ -306,10 +306,6 @@ func main() {
 			if crafted := source.GetCrafted(); crafted != nil {
 				craftedSpellIds = append(craftedSpellIds, crafted.SpellId)
 			}
-			// Add Eye Of The Black Prince gem socket to Throne of Thunder weapons.
-			if drop := source.GetDrop(); drop != nil && (item.Type == proto.ItemType_ItemTypeWeapon || item.Type == proto.ItemType_ItemTypeRanged) && (item.WeaponType != proto.WeaponType_WeaponTypeOffHand && item.WeaponType != proto.WeaponType_WeaponTypeShield) && drop.ZoneId == 6622 {
-				item.GemSockets = append(item.GemSockets, proto.GemColor_GemColorPrismatic)
-			}
 		}
 
 		if item.Phase < 2 {
