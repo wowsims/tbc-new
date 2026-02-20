4850b6d77854788499619e971be9ffb31bb5f642
Adrian Klingen <adrian.klingen@deptagency.com>
2026-02-20 12:54:30 +0100

    Magtheridon encounter improvements

--- sim/encounters/magtheridons_lair/magtheridon_ai.go
+++ sim/encounters/magtheridons_lair/magtheridon_ai.go
@@ -66,7 +66,10 @@ type MagtheridonAI struct {
 	ValidTanks []*core.Unit
 
 	// Spell + aura references
-	Cleave *core.Spell
+	Cleave          *core.Spell
+	Quake           *core.Spell
+	BlastNova       *core.Spell
+	ShadowGraspAura *core.Aura
 }
 
 func (ai *MagtheridonAI) Initialize(target *core.Target, config *proto.Target) {
@@ -83,9 +86,98 @@ func (ai *MagtheridonAI) Initialize(target *core.Target, config *proto.Target) {
 	})
 
 	// Register relevant spells and auras
+	ai.registerBlastNova()
+	ai.registerQuake()
 	ai.registerCleave()
 
 	ai.BossUnit.AutoAttacks.SetReplaceMHSwing(ai.TryCleave)
+}
+
+func (ai *MagtheridonAI) registerBlastNova() {
+	ai.ShadowGraspAura = ai.BossUnit.RegisterAura(core.Aura{
+		Label:    "Shadow Grasp",
+		ActionID: core.ActionID{SpellID: 30410},
+		Duration: time.Second * 10,
+	}).
+		AttachMultiplicativePseudoStatBuff(&ai.BossUnit.PseudoStats.DamageTakenMultiplier, 3)
+
+	ai.BlastNova = ai.BossUnit.RegisterSpell(core.SpellConfig{
+		ActionID:    core.ActionID{SpellID: 30616},
+		SpellSchool: core.SpellSchoolFire,
+
+		Cast: core.CastConfig{
+			DefaultCast: core.Cast{
+				GCD:      core.BossGCD,
+				CastTime: time.Second * 2,
+			},
+
+			ModifyCast: func(sim *core.Simulation, spell *core.Spell, cast *core.Cast) {
+				cast.CastTime = core.DurationFromSeconds(sim.Roll(0.2, 2))
+				meleeDelay := sim.CurrentTime + cast.CastTime + ai.ShadowGraspAura.Duration
+				spell.Unit.AutoAttacks.StopMeleeUntil(sim, meleeDelay)
+				cleaveDelay := max(
+					spell.Unit.AutoAttacks.MainhandSwingSpeed()+1,
+					core.DurationFromSeconds(sim.RandomFloat("Cleave delay")*ai.Cleave.CD.Duration.Seconds()),
+				)
+				ai.Cleave.CD.Set(meleeDelay + cleaveDelay)
+			},
+
+			CD: core.Cooldown{
+				Timer:    ai.BossUnit.NewTimer(),
+				Duration: time.Minute * 1,
+			},
+
+			IgnoreHaste: true,
+		},
+
+		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+			spell.CalcAndDealOutcome(sim, target, spell.OutcomeAlwaysMiss)
+			ai.ShadowGraspAura.Activate(sim)
+		},
+	})
+
+	ai.BossUnit.RegisterResetEffect(func(sim *core.Simulation) {
+		ai.BlastNova.CD.Set(core.DurationFromSeconds(sim.RandomFloat("Blast Nova Timing")) + ai.BlastNova.CD.Duration)
+	})
+}
+
+func (ai *MagtheridonAI) registerQuake() {
+	ai.Quake = ai.BossUnit.RegisterSpell(core.SpellConfig{
+		ActionID:    core.ActionID{SpellID: 30576},
+		SpellSchool: core.SpellSchoolPhysical,
+
+		Cast: core.CastConfig{
+			DefaultCast: core.Cast{
+				GCD:      core.BossGCD,
+				CastTime: time.Second * 7,
+			},
+
+			ModifyCast: func(sim *core.Simulation, spell *core.Spell, cast *core.Cast) {
+				meleeDelay := sim.CurrentTime + cast.CastTime
+				spell.Unit.AutoAttacks.StopMeleeUntil(sim, meleeDelay)
+				cleaveDelay := max(
+					spell.Unit.AutoAttacks.MainhandSwingSpeed()+1,
+					core.DurationFromSeconds(sim.RandomFloat("Cleave delay")*spell.Unit.AutoAttacks.MainhandSwingSpeed().Seconds()*3),
+				)
+				ai.Cleave.CD.Set(meleeDelay + cleaveDelay)
+			},
+
+			CD: core.Cooldown{
+				Timer:    ai.BossUnit.NewTimer(),
+				Duration: time.Minute * 1,
+			},
+
+			IgnoreHaste: true,
+		},
+
+		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+			spell.CalcAndDealOutcome(sim, target, spell.OutcomeAlwaysHit)
+		},
+	})
+
+	ai.BossUnit.RegisterResetEffect(func(sim *core.Simulation) {
+		ai.Quake.CD.Set(core.DurationFromSeconds(sim.RandomFloat("Quake Timing")) + time.Second*40)
+	})
 }
 
 func (ai *MagtheridonAI) registerCleave() {
@@ -107,13 +199,12 @@ func (ai *MagtheridonAI) registerCleave() {
 
 		ApplyEffects: func(sim *core.Simulation, tankTarget *core.Unit, spell *core.Spell) {
 			baseDamage := spell.Unit.AutoAttacks.MH().EnemyWeaponDamage(sim, spell.MeleeAttackPower(), magtheridonMeleeDamageSpread)
-
 			spell.CalcAndDealDamage(sim, tankTarget, baseDamage, spell.OutcomeEnemyMeleeWhite)
 		},
 	})
 
 	ai.BossUnit.RegisterResetEffect(func(sim *core.Simulation) {
-		ai.Cleave.CD.Set(core.DurationFromSeconds(sim.RandomFloat("Cleave Timing") * ai.Cleave.CD.Duration.Seconds()))
+		ai.Cleave.CD.Set(core.DurationFromSeconds(sim.RandomFloat("Cleave Timing")) + ai.Cleave.CD.Duration)
 	})
 }
 
@@ -127,6 +218,14 @@ func (ai *MagtheridonAI) ExecuteCustomRotation(sim *core.Simulation) {
 	if target == nil {
 		// For individual non tank sims we still want abilities to work
 		target = &ai.Target.Env.Raid.Parties[0].Players[0].GetCharacter().Unit
+	}
+
+	if ai.BlastNova.CanCast(sim, target) {
+		ai.BlastNova.Cast(sim, target)
+	}
+
+	if ai.Quake.CanCast(sim, target) {
+		ai.Quake.Cast(sim, target)
 	}
 
 	ai.Target.ExtendGCDUntil(sim, sim.CurrentTime+core.BossGCD)
