wowsims/mop

db2tool: pure-Go DB2 extractor core (dbd parser, WDC5 decoder, sqlite writer)
First stage of docs/db2tool-migration-plan.md: everything downstream of
file extraction, fed by pre-extracted .db2 files.

- dbd/: BSD-3 port of DBDefsLib's DBDReader + the SQLite helpers' exact-build
  version selection (last block whose builds contain the trailing build
  number; fail-loud when WoWDBDefs lacks the build).
- wdc/: MIT port of DBCD.IO's WDC5Reader/WDC4Row/BitReader — multi-section,
  all 6 compression modes, sparse offset-map, copy tables, trailing
  relations, negative-base string offsets, and the keyless encrypted-section
  skip (TactKeyLookup != 0 + zero-filled data).
- sqlite/: port of this repo's SQLiteDbCreator/SqliteDataInserter via
  modernc.org/sqlite (delete-first lifecycle, generated [Col_i] VIRTUAL
  columns, table-less idx_ names in settings order, relation 0 stays 0,
  float32 marshaling, C# Array-declared JSON serialization semantics).
- Temporary driver (main.go) decodes tools/DB2ToSqlite/dbfilesclient against
  DBDCache for an explicit --build; a follow-up commit replaces this with
  local CASC extraction.
- internal/golden + tests: schema DDL gate, per-table row parity, the
  float-notation assertion from plan §5.5, modernc marshaling smoke test.

Validation vs the Jul 11 dotnet reference (build 5.5.4.68571, with
hotfixes): sqlite_master schema byte-identical (236 objects); row dumps
byte-identical for 67/72 tables; the 9 differing lines are exactly the
DBCache hotfix overlay (add item 272920/spell 1291315 'Spring Panda',
delete item 277947, modify spell 1298412) plus the documented CurvePoint
float-notation case (slack table). gen_db against the Go-built DB
reproduces the committed assets/database/db.json and leftover_db.json
byte-for-byte (.bin files are nondeterministic even from the reference DB).

tools/db2tool/sqlite/insert.go
@@ -1,0 +1,135 @@
+// Port of this repo's tools/DB2ToSqlite/Helpers/SqliteDataInserter.cs.
+// Original repo code (MIT), no external attribution owed.
+package sqlite
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"math"
+	"strings"
+
+	"github.com/wowsims/mop/tools/db2tool/wdc"
+)
+
+// InsertRows upserts every decoded row of one table inside one transaction.
+//
+// Contract notes (plan §5.4/§5.5, all verified against the reference DB):
+//   - definition order = column order = bind order;
+//   - relation-column idx_ indexes use table-less names (idx_<col lowercased>)
+//     with IF NOT EXISTS, so only the first table processed with a given
+//     relation-column name gets the index — tables MUST be processed in
+//     settings order;
+//   - relation values of 0 stay 0 (the C# 0→NULL branch is a boxed reference
+//     comparison that is always false — dead code);
+//   - arrays serialize as JSON text via encoding/json over plain numeric
+//     slices, matching C# System.Text.Json's Array-declared serialization
+//     (boxed elements — u8 arrays emit [0,0,0], never base64);
+//   - float scalars bind as the double-widened float32.
+func InsertRows(db *sql.DB, t TableDef, decoded *wdc.Decoded) error {
+	defs := t.Version.Definitions
+
+	columnNames := make([]string, len(defs))
+	for i, d := range defs {
+		columnNames[i] = d.Name
+	}
+
+	pkColumn := ""
+	for _, d := range defs {
+		if d.IsID {
+			pkColumn = d.Name
+			break
+		}
+	}
+	if pkColumn == "" {
+		return fmt.Errorf("table %s has no id column", t.Name)
+	}
+
+	var cols, vals []string
+	for _, c := range columnNames {
+		cols = append(cols, "["+c+"]")
+		vals = append(vals, "@"+c)
+	}
+
+	var updates []string
+	for _, c := range columnNames {
+		if c != pkColumn {
+			updates = append(updates, fmt.Sprintf("[%s] = excluded.[%s]", c, c))
+		}
+	}
+	updateClause := "DO NOTHING"
+	if len(updates) > 0 {
+		updateClause = "DO UPDATE SET " + strings.Join(updates, ", ")
+	}
+
+	upsertSql := fmt.Sprintf("INSERT INTO [%s] (%s) VALUES (%s) ON CONFLICT([%s]) %s;",
+		t.Name, strings.Join(cols, ", "), strings.Join(vals, ", "), pkColumn, updateClause)
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	// Relation-column indexes, created before the inserts like the C# tool.
+	for _, d := range defs {
+		if d.IsRelation {
+			stmt := fmt.Sprintf("CREATE INDEX IF NOT EXISTS idx_%s ON %s (%s);", strings.ToLower(d.Name), t.Name, d.Name)
+			if _, err := tx.Exec(stmt); err != nil {
+				return fmt.Errorf("creating relation index on %s.%s: %w", t.Name, d.Name, err)
+			}
+		}
+	}
+
+	stmt, err := tx.Prepare(upsertSql)
+	if err != nil {
+		return fmt.Errorf("preparing upsert for %s: %w", t.Name, err)
+	}
+	defer stmt.Close()
+
+	args := make([]any, len(defs))
+	for _, row := range decoded.Rows {
+		if len(row.Values) != len(defs) {
+			return fmt.Errorf("table %s row %d: %d values for %d definitions", t.Name, row.ID, len(row.Values), len(defs))
+		}
+		for i, value := range row.Values {
+			bound, err := bindValue(value)
+			if err != nil {
+				return fmt.Errorf("table %s row %d column %s: %w", t.Name, row.ID, defs[i].Name, err)
+			}
+			args[i] = sql.Named(defs[i].Name, bound)
+		}
+		if _, err := stmt.Exec(args...); err != nil {
+			return fmt.Errorf("inserting row %d into %s: %w", row.ID, t.Name, err)
+		}
+	}
+
+	return tx.Commit()
+}
+
+// bindValue converts a decoded value into a driver-bindable one.
+func bindValue(value any) (any, error) {
+	switch v := value.(type) {
+	case nil:
+		return nil, nil
+	case int64:
+		return v, nil
+	case uint64:
+		if v > math.MaxInt64 {
+			return nil, fmt.Errorf("uint64 value %d overflows INTEGER", v)
+		}
+		return int64(v), nil
+	case float32:
+		return float64(v), nil // double-widened, same as Microsoft.Data.Sqlite
+	case string:
+		return v, nil
+	case []int64, []uint64, []float32, []string:
+		b, err := json.Marshal(v)
+		if err != nil {
+			return nil, err
+		}
+		return string(b), nil
+	default:
+		return nil, fmt.Errorf("unsupported value type %T", value)
+	}
+}

tools/db2tool/sqlite/schema.go
@@ -1,0 +1,125 @@
+// Port of this repo's tools/DB2ToSqlite/Helpers/SQLiteDbCreator.cs.
+// Original repo code (MIT), no external attribution owed.
+package sqlite
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/wowsims/mop/tools/db2tool/dbd"
+)
+
+// TableDef pairs a table name with its parsed definition and the version
+// block selected for the current build (plan §5.1 selection rule; the caller
+// selects once and both schema and inserts use the same block).
+type TableDef struct {
+	Name    string
+	Def     dbd.DBDefinition
+	Version dbd.VersionDefinitions
+}
+
+// Open deletes any pre-existing database file (SQLiteDbCreator.cs:11 — every
+// run starts from an empty file; this is what makes post-patch re-runs and
+// db/ptrdb alternation correct, plan §5) and opens a fresh connection with
+// PRAGMA foreign_keys = ON.
+func Open(path string) (*sql.DB, error) {
+	if _, err := os.Stat(path); err == nil {
+		if err := os.Remove(path); err != nil {
+			return nil, fmt.Errorf("deleting existing database: %w", err)
+		}
+	}
+	db, err := sql.Open("sqlite", path)
+	if err != nil {
+		return nil, err
+	}
+	// The writer is single-threaded; a single connection keeps transaction
+	// semantics identical to the C# tool's one SqliteConnection.
+	db.SetMaxOpenConns(1)
+	if _, err := db.Exec("PRAGMA foreign_keys = ON;"); err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
+}
+
+// CreateTables emits the schema for every table, in order, inside one
+// transaction — a transcription of SQLiteDbCreator.CreateDatabaseWithDefinitions.
+func CreateTables(db *sql.DB, tables []TableDef) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	for _, t := range tables {
+		var columnDefinitionsSql []string
+		var indexSql []string
+
+		for _, def := range t.Version.Definitions {
+			colDef, ok := t.Def.ColumnDefinitions[def.Name]
+			if !ok {
+				return fmt.Errorf("column definition for %s not found in table %s", def.Name, t.Name)
+			}
+
+			if def.ArrLength == 0 {
+				sqliteType, err := mapToSQLiteType(colDef.Type)
+				if err != nil {
+					return fmt.Errorf("table %s column %s: %w", t.Name, def.Name, err)
+				}
+				nullability := ""
+				if colDef.ForeignTable != "" && colDef.ForeignColumn != "" && !def.IsID {
+					nullability = " NULL"
+				}
+				columnSql := fmt.Sprintf("[%s] %s%s", def.Name, sqliteType, nullability)
+				if def.IsID {
+					columnSql += " PRIMARY KEY"
+				}
+				columnDefinitionsSql = append(columnDefinitionsSql, columnSql)
+
+				if colDef.ForeignTable != "" && colDef.ForeignColumn != "" {
+					indexSql = append(indexSql, fmt.Sprintf(
+						"CREATE INDEX IF NOT EXISTS IX_%s_%s ON [%s] ([%s])", t.Name, def.Name, t.Name, def.Name))
+				}
+			} else {
+				columnDefinitionsSql = append(columnDefinitionsSql, fmt.Sprintf("[%s] TEXT", def.Name))
+
+				elementType, err := mapToSQLiteType(colDef.Type)
+				if err != nil {
+					return fmt.Errorf("table %s column %s: %w", t.Name, def.Name, err)
+				}
+				for i := 0; i < def.ArrLength; i++ {
+					columnDefinitionsSql = append(columnDefinitionsSql, fmt.Sprintf(
+						"[%s_%d] %s GENERATED ALWAYS AS (json_extract([%s], '$[%d]')) VIRTUAL",
+						def.Name, i, elementType, def.Name, i))
+				}
+			}
+		}
+
+		createTableSql := fmt.Sprintf("CREATE TABLE IF NOT EXISTS [%s] (%s);", t.Name, strings.Join(columnDefinitionsSql, ", "))
+		if _, err := tx.Exec(createTableSql); err != nil {
+			return fmt.Errorf("creating table %s: %w", t.Name, err)
+		}
+		for _, stmt := range indexSql {
+			if _, err := tx.Exec(stmt); err != nil {
+				return fmt.Errorf("creating index on %s: %w", t.Name, err)
+			}
+		}
+	}
+
+	return tx.Commit()
+}
+
+func mapToSQLiteType(colType string) (string, error) {
+	switch colType {
+	case "int", "uint":
+		return "INTEGER", nil
+	case "float":
+		return "REAL", nil
+	case "string", "locstring":
+		return "TEXT", nil
+	default:
+		return "", fmt.Errorf("unsupported type: %s", colType)
+	}
+}

tools/db2tool/wdc/wdc5_test.go
@@ -1,0 +1,123 @@
+package wdc
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/wowsims/mop/tools/db2tool/dbd"
+)
+
+// Pre-port audit fixture (plan §8 step 2), frozen from the build-68571
+// snapshot in tools/DB2ToSqlite/dbfilesclient. Tests skip when the gitignored
+// snapshot is absent.
+const db2Dir = "../../DB2ToSqlite/dbfilesclient"
+const dbdDir = "../../DB2ToSqlite/DBDCache"
+const snapshotBuild = 68571
+
+func db2Files(t *testing.T) []string {
+	t.Helper()
+	entries, err := os.ReadDir(db2Dir)
+	if os.IsNotExist(err) {
+		t.Skipf("%s not present (gitignored snapshot); skipping", db2Dir)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	var files []string
+	for _, e := range entries {
+		if strings.HasSuffix(e.Name(), ".db2") {
+			files = append(files, e.Name())
+		}
+	}
+	if len(files) != 72 {
+		t.Fatalf("expected 72 .db2 files, got %d", len(files))
+	}
+	return files
+}
+
+func TestParseAllHeaders(t *testing.T) {
+	sectionCounts := map[int]bool{}
+	for _, name := range db2Files(t) {
+		table, err := ReadFile(filepath.Join(db2Dir, name))
+		if err != nil {
+			t.Fatalf("%s: %v", name, err)
+		}
+		sectionCounts[len(table.Sections)] = true
+
+		base := strings.TrimSuffix(name, ".db2")
+		switch base {
+		case "ItemBonus":
+			if len(table.Sections) != 0 && table.RecordsCount != 0 {
+				t.Errorf("ItemBonus: expected empty table, got %d sections / %d records", len(table.Sections), table.RecordsCount)
+			}
+		case "Spell", "ItemSparse":
+			if table.Flags != 0x5 {
+				t.Errorf("%s: Flags = 0x%x, want 0x5 (Sparse|Index)", base, table.Flags)
+			}
+		}
+		if table.Flags&flagSecondaryKey != 0 {
+			t.Errorf("%s: unexpected SecondaryKey flag", base)
+		}
+	}
+	// Distinct section counts frozen in the plan (§8 step 2), plus 0 for the
+	// empty ItemBonus and 1 for plain single-section tables.
+	for _, want := range []int{36, 33, 26, 22, 16, 9, 8, 3, 2, 1, 0} {
+		if !sectionCounts[want] {
+			t.Errorf("expected some table to have %d sections", want)
+		}
+	}
+}
+
+func TestSpellEffectEncryptedSkip(t *testing.T) {
+	if _, err := os.Stat(db2Dir); os.IsNotExist(err) {
+		t.Skip("snapshot not present")
+	}
+	table, err := ReadFile(filepath.Join(db2Dir, "SpellEffect.db2"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if table.RecordsCount != 142756 {
+		t.Errorf("SpellEffect header record_count = %d, want 142756", table.RecordsCount)
+	}
+	if len(table.Sections) != 36 {
+		t.Errorf("SpellEffect sections = %d, want 36", len(table.Sections))
+	}
+	if table.SkippedSections != 35 {
+		t.Errorf("SpellEffect skipped sections = %d, want 35", table.SkippedSections)
+	}
+	// C1 exact check: 142756 header records − 136 encrypted = 142620 emitted.
+	if got := len(table.rows); got != 142620 {
+		t.Errorf("SpellEffect decoded raw rows = %d, want 142620", got)
+	}
+}
+
+func TestDecodeAllTables(t *testing.T) {
+	if _, err := os.Stat(dbdDir); os.IsNotExist(err) {
+		t.Skip("snapshot not present")
+	}
+	for _, name := range db2Files(t) {
+		base := strings.TrimSuffix(name, ".db2")
+		table, err := ReadFile(filepath.Join(db2Dir, name))
+		if err != nil {
+			t.Fatalf("%s: %v", name, err)
+		}
+		def, err := dbd.ReadFile(filepath.Join(dbdDir, base+".dbd"), true)
+		if err != nil {
+			t.Fatalf("%s: %v", base, err)
+		}
+		version, err := dbd.SelectVersion(def, snapshotBuild)
+		if err != nil {
+			t.Fatalf("%s: %v", base, err)
+		}
+		decoded, err := table.DecodeRows(def, version, snapshotBuild)
+		if err != nil {
+			t.Fatalf("%s: decode: %v", base, err)
+		}
+		if base != "ItemBonus" && len(decoded.Rows) == 0 {
+			t.Errorf("%s: decoded 0 rows", base)
+		}
+		t.Logf("%s: %d rows, %d cols, %d skipped sections", base, len(decoded.Rows), len(decoded.ColumnNames), table.SkippedSections)
+	}
+}

wowsims/tbc-new

tools/db2tool/sqlite/insert.go
@@ -1,0 +1,135 @@
+// Port of this repo's tools/DB2ToSqlite/Helpers/SqliteDataInserter.cs.
+// Original repo code (MIT), no external attribution owed.
+package sqlite
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"math"
+	"strings"
+
+	"github.com/wowsims/tbc/tools/db2tool/wdc"
+)
+
+// InsertRows upserts every decoded row of one table inside one transaction.
+//
+// Contract notes (plan §5.4/§5.5, all verified against the reference DB):
+//   - definition order = column order = bind order;
+//   - relation-column idx_ indexes use table-less names (idx_<col lowercased>)
+//     with IF NOT EXISTS, so only the first table processed with a given
+//     relation-column name gets the index — tables MUST be processed in
+//     settings order;
+//   - relation values of 0 stay 0 (the C# 0→NULL branch is a boxed reference
+//     comparison that is always false — dead code);
+//   - arrays serialize as JSON text via encoding/json over plain numeric
+//     slices, matching C# System.Text.Json's Array-declared serialization
+//     (boxed elements — u8 arrays emit [0,0,0], never base64);
+//   - float scalars bind as the double-widened float32.
+func InsertRows(db *sql.DB, t TableDef, decoded *wdc.Decoded) error {
+	defs := t.Version.Definitions
+
+	columnNames := make([]string, len(defs))
+	for i, d := range defs {
+		columnNames[i] = d.Name
+	}
+
+	pkColumn := ""
+	for _, d := range defs {
+		if d.IsID {
+			pkColumn = d.Name
+			break
+		}
+	}
+	if pkColumn == "" {
+		return fmt.Errorf("table %s has no id column", t.Name)
+	}
+
+	var cols, vals []string
+	for _, c := range columnNames {
+		cols = append(cols, "["+c+"]")
+		vals = append(vals, "@"+c)
+	}
+
+	var updates []string
+	for _, c := range columnNames {
+		if c != pkColumn {
+			updates = append(updates, fmt.Sprintf("[%s] = excluded.[%s]", c, c))
+		}
+	}
+	updateClause := "DO NOTHING"
+	if len(updates) > 0 {
+		updateClause = "DO UPDATE SET " + strings.Join(updates, ", ")
+	}
+
+	upsertSql := fmt.Sprintf("INSERT INTO [%s] (%s) VALUES (%s) ON CONFLICT([%s]) %s;",
+		t.Name, strings.Join(cols, ", "), strings.Join(vals, ", "), pkColumn, updateClause)
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	// Relation-column indexes, created before the inserts like the C# tool.
+	for _, d := range defs {
+		if d.IsRelation {
+			stmt := fmt.Sprintf("CREATE INDEX IF NOT EXISTS idx_%s ON %s (%s);", strings.ToLower(d.Name), t.Name, d.Name)
+			if _, err := tx.Exec(stmt); err != nil {
+				return fmt.Errorf("creating relation index on %s.%s: %w", t.Name, d.Name, err)
+			}
+		}
+	}
+
+	stmt, err := tx.Prepare(upsertSql)
+	if err != nil {
+		return fmt.Errorf("preparing upsert for %s: %w", t.Name, err)
+	}
+	defer stmt.Close()
+
+	args := make([]any, len(defs))
+	for _, row := range decoded.Rows {
+		if len(row.Values) != len(defs) {
+			return fmt.Errorf("table %s row %d: %d values for %d definitions", t.Name, row.ID, len(row.Values), len(defs))
+		}
+		for i, value := range row.Values {
+			bound, err := bindValue(value)
+			if err != nil {
+				return fmt.Errorf("table %s row %d column %s: %w", t.Name, row.ID, defs[i].Name, err)
+			}
+			args[i] = sql.Named(defs[i].Name, bound)
+		}
+		if _, err := stmt.Exec(args...); err != nil {
+			return fmt.Errorf("inserting row %d into %s: %w", row.ID, t.Name, err)
+		}
+	}
+
+	return tx.Commit()
+}
+
+// bindValue converts a decoded value into a driver-bindable one.
+func bindValue(value any) (any, error) {
+	switch v := value.(type) {
+	case nil:
+		return nil, nil
+	case int64:
+		return v, nil
+	case uint64:
+		if v > math.MaxInt64 {
+			return nil, fmt.Errorf("uint64 value %d overflows INTEGER", v)
+		}
+		return int64(v), nil
+	case float32:
+		return float64(v), nil // double-widened, same as Microsoft.Data.Sqlite
+	case string:
+		return v, nil
+	case []int64, []uint64, []float32, []string:
+		b, err := json.Marshal(v)
+		if err != nil {
+			return nil, err
+		}
+		return string(b), nil
+	default:
+		return nil, fmt.Errorf("unsupported value type %T", value)
+	}
+}

tools/db2tool/sqlite/schema.go
@@ -1,0 +1,125 @@
+// Port of this repo's tools/DB2ToSqlite/Helpers/SQLiteDbCreator.cs.
+// Original repo code (MIT), no external attribution owed.
+package sqlite
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/wowsims/tbc/tools/db2tool/dbd"
+)
+
+// TableDef pairs a table name with its parsed definition and the version
+// block selected for the current build (plan §5.1 selection rule; the caller
+// selects once and both schema and inserts use the same block).
+type TableDef struct {
+	Name    string
+	Def     dbd.DBDefinition
+	Version dbd.VersionDefinitions
+}
+
+// Open deletes any pre-existing database file (SQLiteDbCreator.cs:11 — every
+// run starts from an empty file; this is what makes post-patch re-runs and
+// db/ptrdb alternation correct, plan §5) and opens a fresh connection with
+// PRAGMA foreign_keys = ON.
+func Open(path string) (*sql.DB, error) {
+	if _, err := os.Stat(path); err == nil {
+		if err := os.Remove(path); err != nil {
+			return nil, fmt.Errorf("deleting existing database: %w", err)
+		}
+	}
+	db, err := sql.Open("sqlite", path)
+	if err != nil {
+		return nil, err
+	}
+	// The writer is single-threaded; a single connection keeps transaction
+	// semantics identical to the C# tool's one SqliteConnection.
+	db.SetMaxOpenConns(1)
+	if _, err := db.Exec("PRAGMA foreign_keys = ON;"); err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
+}
+
+// CreateTables emits the schema for every table, in order, inside one
+// transaction — a transcription of SQLiteDbCreator.CreateDatabaseWithDefinitions.
+func CreateTables(db *sql.DB, tables []TableDef) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	for _, t := range tables {
+		var columnDefinitionsSql []string
+		var indexSql []string
+
+		for _, def := range t.Version.Definitions {
+			colDef, ok := t.Def.ColumnDefinitions[def.Name]
+			if !ok {
+				return fmt.Errorf("column definition for %s not found in table %s", def.Name, t.Name)
+			}
+
+			if def.ArrLength == 0 {
+				sqliteType, err := mapToSQLiteType(colDef.Type)
+				if err != nil {
+					return fmt.Errorf("table %s column %s: %w", t.Name, def.Name, err)
+				}
+				nullability := ""
+				if colDef.ForeignTable != "" && colDef.ForeignColumn != "" && !def.IsID {
+					nullability = " NULL"
+				}
+				columnSql := fmt.Sprintf("[%s] %s%s", def.Name, sqliteType, nullability)
+				if def.IsID {
+					columnSql += " PRIMARY KEY"
+				}
+				columnDefinitionsSql = append(columnDefinitionsSql, columnSql)
+
+				if colDef.ForeignTable != "" && colDef.ForeignColumn != "" {
+					indexSql = append(indexSql, fmt.Sprintf(
+						"CREATE INDEX IF NOT EXISTS IX_%s_%s ON [%s] ([%s])", t.Name, def.Name, t.Name, def.Name))
+				}
+			} else {
+				columnDefinitionsSql = append(columnDefinitionsSql, fmt.Sprintf("[%s] TEXT", def.Name))
+
+				elementType, err := mapToSQLiteType(colDef.Type)
+				if err != nil {
+					return fmt.Errorf("table %s column %s: %w", t.Name, def.Name, err)
+				}
+				for i := 0; i < def.ArrLength; i++ {
+					columnDefinitionsSql = append(columnDefinitionsSql, fmt.Sprintf(
+						"[%s_%d] %s GENERATED ALWAYS AS (json_extract([%s], '$[%d]')) VIRTUAL",
+						def.Name, i, elementType, def.Name, i))
+				}
+			}
+		}
+
+		createTableSql := fmt.Sprintf("CREATE TABLE IF NOT EXISTS [%s] (%s);", t.Name, strings.Join(columnDefinitionsSql, ", "))
+		if _, err := tx.Exec(createTableSql); err != nil {
+			return fmt.Errorf("creating table %s: %w", t.Name, err)
+		}
+		for _, stmt := range indexSql {
+			if _, err := tx.Exec(stmt); err != nil {
+				return fmt.Errorf("creating index on %s: %w", t.Name, err)
+			}
+		}
+	}
+
+	return tx.Commit()
+}
+
+func mapToSQLiteType(colType string) (string, error) {
+	switch colType {
+	case "int", "uint":
+		return "INTEGER", nil
+	case "float":
+		return "REAL", nil
+	case "string", "locstring":
+		return "TEXT", nil
+	default:
+		return "", fmt.Errorf("unsupported type: %s", colType)
+	}
+}

tools/db2tool/wdc/wdc5_test.go
@@ -1,0 +1,123 @@
+package wdc
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/wowsims/tbc/tools/db2tool/dbd"
+)
+
+// Pre-port audit fixture (plan §8 step 2), frozen from the build-68571
+// snapshot in tools/DB2ToSqlite/dbfilesclient. Tests skip when the gitignored
+// snapshot is absent.
+const db2Dir = "../../DB2ToSqlite/dbfilesclient"
+const dbdDir = "../../DB2ToSqlite/DBDCache"
+const snapshotBuild = 68571
+
+func db2Files(t *testing.T) []string {
+	t.Helper()
+	entries, err := os.ReadDir(db2Dir)
+	if os.IsNotExist(err) {
+		t.Skipf("%s not present (gitignored snapshot); skipping", db2Dir)
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+	var files []string
+	for _, e := range entries {
+		if strings.HasSuffix(e.Name(), ".db2") {
+			files = append(files, e.Name())
+		}
+	}
+	if len(files) != 72 {
+		t.Fatalf("expected 72 .db2 files, got %d", len(files))
+	}
+	return files
+}
+
+func TestParseAllHeaders(t *testing.T) {
+	sectionCounts := map[int]bool{}
+	for _, name := range db2Files(t) {
+		table, err := ReadFile(filepath.Join(db2Dir, name))
+		if err != nil {
+			t.Fatalf("%s: %v", name, err)
+		}
+		sectionCounts[len(table.Sections)] = true
+
+		base := strings.TrimSuffix(name, ".db2")
+		switch base {
+		case "ItemBonus":
+			if len(table.Sections) != 0 && table.RecordsCount != 0 {
+				t.Errorf("ItemBonus: expected empty table, got %d sections / %d records", len(table.Sections), table.RecordsCount)
+			}
+		case "Spell", "ItemSparse":
+			if table.Flags != 0x5 {
+				t.Errorf("%s: Flags = 0x%x, want 0x5 (Sparse|Index)", base, table.Flags)
+			}
+		}
+		if table.Flags&flagSecondaryKey != 0 {
+			t.Errorf("%s: unexpected SecondaryKey flag", base)
+		}
+	}
+	// Distinct section counts frozen in the plan (§8 step 2), plus 0 for the
+	// empty ItemBonus and 1 for plain single-section tables.
+	for _, want := range []int{36, 33, 26, 22, 16, 9, 8, 3, 2, 1, 0} {
+		if !sectionCounts[want] {
+			t.Errorf("expected some table to have %d sections", want)
+		}
+	}
+}
+
+func TestSpellEffectEncryptedSkip(t *testing.T) {
+	if _, err := os.Stat(db2Dir); os.IsNotExist(err) {
+		t.Skip("snapshot not present")
+	}
+	table, err := ReadFile(filepath.Join(db2Dir, "SpellEffect.db2"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if table.RecordsCount != 142756 {
+		t.Errorf("SpellEffect header record_count = %d, want 142756", table.RecordsCount)
+	}
+	if len(table.Sections) != 36 {
+		t.Errorf("SpellEffect sections = %d, want 36", len(table.Sections))
+	}
+	if table.SkippedSections != 35 {
+		t.Errorf("SpellEffect skipped sections = %d, want 35", table.SkippedSections)
+	}
+	// C1 exact check: 142756 header records − 136 encrypted = 142620 emitted.
+	if got := len(table.rows); got != 142620 {
+		t.Errorf("SpellEffect decoded raw rows = %d, want 142620", got)
+	}
+}
+
+func TestDecodeAllTables(t *testing.T) {
+	if _, err := os.Stat(dbdDir); os.IsNotExist(err) {
+		t.Skip("snapshot not present")
+	}
+	for _, name := range db2Files(t) {
+		base := strings.TrimSuffix(name, ".db2")
+		table, err := ReadFile(filepath.Join(db2Dir, name))
+		if err != nil {
+			t.Fatalf("%s: %v", name, err)
+		}
+		def, err := dbd.ReadFile(filepath.Join(dbdDir, base+".dbd"), true)
+		if err != nil {
+			t.Fatalf("%s: %v", base, err)
+		}
+		version, err := dbd.SelectVersion(def, snapshotBuild)
+		if err != nil {
+			t.Fatalf("%s: %v", base, err)
+		}
+		decoded, err := table.DecodeRows(def, version, snapshotBuild)
+		if err != nil {
+			t.Fatalf("%s: decode: %v", base, err)
+		}
+		if base != "ItemBonus" && len(decoded.Rows) == 0 {
+			t.Errorf("%s: decoded 0 rows", base)
+		}
+		t.Logf("%s: %d rows, %d cols, %d skipped sections", base, len(decoded.Rows), len(decoded.ColumnNames), table.SkippedSections)
+	}
+}